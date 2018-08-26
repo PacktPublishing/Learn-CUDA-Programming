0d4efdeb27b4fdf12341788a2ba1de0814059bab
Jack Han <jahan@nvidia.com>
2018-08-27 02:49:30 +0900

    changed memory loading & added extra register use

---
+++ 03_cuda_thread_programming/06_limiter_balancing/reduction_kernel_extra.cu
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "reduction.h"
+
+#define NUM_LOAD 4
+
+/*
+    Parallel sum reduction using shared memory
+    - takes log(n) steps for n input elements
+    - uses n threads
+    - only works for power-of-2 arrays
+*/
+__global__ void
+reduction_kernel(float *g_out, float *g_in, unsigned int size)
+{
+    unsigned int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+
+    extern __shared__ float s_data[];
+
+    // cumulates input with grid-stride loop and save to share memory
+    float input[NUM_LOAD] = {0.f};
+    for (int i = idx_x; i < size; i += blockDim.x * gridDim.x * NUM_LOAD)
+    {
+        for (int step = 0; step < NUM_LOAD; step++)
+            input[step] += (i + step * blockDim.x * gridDim.x < size) ? g_in[i + step * blockDim.x * gridDim.x] : 0.f;
+    }
+    for (int i = 1; i < NUM_LOAD; i++)
+        input[0] += input[i];
+    s_data[threadIdx.x] = input[0];
+
+    __syncthreads();
+
+    // do reduction
+    for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
+    {
+        if (threadIdx.x < stride) 
+            s_data[threadIdx.x] += s_data[threadIdx.x + stride];
+        
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) {
+        g_out[blockIdx.x] = s_data[0];
+    }
+}
+
+int reduction(float *g_outPtr, float *g_inPtr, int size, int n_threads)
+{
+    int num_sms;
+    int num_blocks_per_sm;
+    cudaDeviceGetAttribute(&num_sms, cudaDevAttrMultiProcessorCount, 0);
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(&num_blocks_per_sm, reduction_kernel, n_threads, n_threads*sizeof(float));
+    int n_blocks = min(num_blocks_per_sm * num_sms, (size + n_threads - 1) / n_threads);
+    
+    reduction_kernel<<<n_blocks, n_threads, n_threads * sizeof(float), 0>>>(g_outPtr, g_inPtr, size);
+    reduction_kernel<<<1, n_threads, n_threads * sizeof(float), 0>>>(g_outPtr, g_inPtr, n_blocks);
+ 
+    return 1;
+}
