b6fb0941a268ce1da4c6bd463f689807da0deb74
Jae Geun Han <haanjack@gmail.com>
2019-08-04 13:58:59 +0900

    Refactor the two versions of gemm operation to work in a single file

--- 07_parallel_programming_pattern/01_sgemm_optimization/sgemm.cu
+++ 07_parallel_programming_pattern/01_sgemm_optimization/sgemm.cu
@@ -2,7 +2,8 @@
 #include <helper_timer.h>
 #include <cuda_profiler_api.h>
 
-#define BLOCK_DIM 16
+#define RESULT_VERIFICATION 0   // change 1 if you want to verify the result
+#define BLOCK_DIM 16    
 
 ////////////////////////////////////////////////////////////////////////////////
 //! Compute reference data set matrix multiply on GPU
@@ -16,8 +17,7 @@
 //! @param alpha      scala value for matrix multiplication
 //! @param beta       scala value for matrix summation with C
 ////////////////////////////////////////////////////////////////////////////////
-
-__global__ void sgemm_kernel(const float *A, const float *B, float *C, int N, int M, int K, float alpha, float beta)
+__global__ void sgemm_kernel(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta)
 {
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -29,17 +29,42 @@ __global__ void sgemm_kernel(const float *A, const float *B, float *C, int N, in
     C[row * N + col] = alpha * element_c + beta * C[row * N + col];
 }
 
+__global__ void sgemm_kernel_v2(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta)
+{
+    int bid_x = blockIdx.x * blockDim.x;
+    int bid_y = blockIdx.y * blockDim.y;
+    int tid_x = threadIdx.x;
+    int tid_y = threadIdx.y;
+
+    float element_c = 0.f;
+    __shared__ float s_tile_A[BLOCK_DIM][BLOCK_DIM];
+    __shared__ float s_tile_B[BLOCK_DIM][BLOCK_DIM];
+
+    // forward tile with tile size in matrix A
+    for (int k = 0; k < K; k += BLOCK_DIM)
+    {
+        s_tile_A[tid_y][tid_x] = A[ (bid_y + tid_y) * K + tid_x + k ]; // Get sub-matrix from A
+        s_tile_B[tid_y][tid_x] = B[ k * N + bid_x + tid_x ]; // Get sub-matrix from B
+
+        __syncthreads();
+
+        // compute gemm operation with tiles
+        for (int e = 0; e < BLOCK_DIM; e++)
+            element_c += s_tile_A[tid_y][e] * s_tile_B[e][tid_x];
+    }
+
+    C[(bid_y + tid_y) * N + (bid_x + tid_x)] = \
+        alpha * element_c + beta * C[(bid_y + tid_y) * N + (bid_x + tid_x)];
+}
+
 void sgemm_gold(const float *A, const float *B, float *C, int M, int N, int K, float alpha, float beta)
 {
     float element_c = 0.f;
-    for (int row = 0; row < M; row++)
-    {
-        for (int col = 0; col < N; col++)
-        {
-            for (int e = 0; e < K; e++)
-	    {
+    for (int row = 0; row < M; row++) {
+        for (int col = 0; col < N; col++) {
+            for (int e = 0; e < K; e++) {
                 element_c += A[row * K + e] * B[e * N + col];
-	    }
+	        }
             C[row * N + col] = alpha * element_c + beta * C[row * N + col];
         }
     }
@@ -47,8 +72,7 @@ void sgemm_gold(const float *A, const float *B, float *C, int M, int N, int K, f
 
 void random_init(float *data, int length)
 {
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         data[i] = (rand() & 0xFF) / (float)RAND_MAX;
     }
 }
@@ -62,14 +86,14 @@ bool value_test(float *a, float *b, int length)
     return true;
 }
 
-int main()
+int main(int c, char *argv[])
 {
     float *A, *B, *C_host, *C_gpu;
     float *d_A, *d_B, *d_C;
-    int N, M, K;
+    int M, N, K;
     float alpha = 2.f;
     float beta = 1.f;
-    int n_iter = 5;
+    int n_iter = 1;
     N = M = K = 2048;
 
     // initialize timer
@@ -77,50 +101,59 @@ int main()
     sdkCreateTimer(&timer);
 
     // allocation of linear memory space
-    A = (float *)malloc(N * K * sizeof(float));
-    B = (float *)malloc(K * M * sizeof(float));
+    A = (float *)malloc(M * K * sizeof(float));
+    B = (float *)malloc(K * N * sizeof(float));
     C_host = (float *)malloc(M * N * sizeof(float));
     C_gpu = (float *)malloc(M * N * sizeof(float));
 
     // allocation of gpu linear memory space
-    cudaMalloc((void **)&d_A, N * K * sizeof(float));
-    cudaMalloc((void **)&d_B, K * M * sizeof(float));
+    cudaMalloc((void **)&d_A, M * K * sizeof(float));
+    cudaMalloc((void **)&d_B, K * N * sizeof(float));
     cudaMalloc((void **)&d_C, M * N * sizeof(float));
 
     // initialize randomized values for memory space
-    random_init(A, N * K);
-    random_init(B, K * M);
+    random_init(A, M * K);
+    random_init(B, K * N);
 
     // profiler will focus from this point
     sdkStartTimer(&timer);
 
     // copy initial value for gpu memory
-    cudaMemcpy(d_A, A, N * K * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, A, K * M * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_A, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, A, K * N * sizeof(float), cudaMemcpyHostToDevice);
 
     // do operation
     dim3 blockDim(BLOCK_DIM, BLOCK_DIM);
     dim3 gridDim((N + BLOCK_DIM - 1) / BLOCK_DIM, (M + BLOCK_DIM - 1) / BLOCK_DIM);
     cudaProfilerStart();
+
     for (int i = 0; i < n_iter; i++) {
         sgemm_kernel<<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K, alpha, beta);
     }
+
+    for (int i = 0; i < n_iter; i++) {
+        sgemm_kernel_v2<<<gridDim, blockDim>>>(d_A, d_B, d_C, M, N, K, alpha, beta);
+    }
+
+    // profiler will stop its focus
+    cudaProfilerStop();
+    
     // measuring the performance
     cudaDeviceSynchronize();
     sdkStopTimer(&timer); // this profiler should be behined of device synchronization
 
+#if (RESULT_VERIFICATION)
     // copy data from the gpu
     cudaMemcpy(C_gpu, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    // profiler will stop its focus
-    cudaProfilerStop();
-
     // compare the result
     sgemm_gold(A, B, C_host, M, N, K, alpha, beta);
+    
     if (value_test(C_host, C_gpu, M * N))
         printf("SUCCESS!!\n");
     else
         printf("Error\n");
+#endif
 
     // terminates allocated gpu memory space
     cudaFree(d_A);
