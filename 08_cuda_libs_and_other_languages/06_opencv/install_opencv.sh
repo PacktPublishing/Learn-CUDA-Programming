3aaf31b94ae74c7e7a7d71f6109b7e7334e96a90
Jack Han <haanjack@gmail.com>
2019-09-23 20:27:27 +0900

    fix opencv install commands

--- 08_cuda_libs_and_other_languages/06_opencv/install_opencv.sh
+++ 08_cuda_libs_and_other_languages/06_opencv/install_opencv.sh
@@ -2,10 +2,11 @@
 # Install dependencies
 sudo apt-get install -y --no-install-recommends \
     cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
-    libgtkglext1 libgtkglext1-dev 
+    libatlas-base-dev gfortran libeigen3-dev \
+    libgtkglext1 libgtkglext1-dev
 
-# We are install OpenCV 4.0.1
-OPENCV_VERSION=4.0.1
+# We are install OpenCV 4.1.1
+OPENCV_VERSION=4.1.1
 OPENCV_DIR=opencv
 
 # Download OpenCV and contrib source codes
@@ -23,13 +24,15 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D WITH_CUDA=ON -D WITH_CUVID=OFF -D BUILD_opencv_cudacodec=OFF \
+    -D ENABLE_PRECOMPILED_HEADERS=OFF \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D WITH_CUDA=ON -D WITH_CUVID=ON -D BUILD_opencv_cudacodec=ON \
     -D ENABLE_FAST_MATH=1 \
     -D CUDA_FAST_MATH=1 \
     -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${OPENCV_VERSION}/modules \
-    -D WITH_CUBLAS=1 \
+    -D WITH_CUBLAS=1 ..
+    -D PYTHON_DEFAULT_EXECUTABLE=`which python3` \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D BUILD_EXAMPLES=ON ..
 make -j$(nproc)
 sudo make install -j$(nproc)
-
